e76193e7714a0915f459534b15a60bda6086f38a
UlasGultekin <gltknulas96@gmail.com>
2025-02-28 02:14:02 +0300

    add catalog

---
+++ src/main/java/com/ulasgltkn/springmicroservicetest/SpringMicroserviceTestApplication.java
@@ -0,0 +1,13 @@
+package com.ulasgltkn.springmicroservicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ulasgltkn/springmicroservicetest/SpringMicroserviceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasgltkn.springmicroservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
